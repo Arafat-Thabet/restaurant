d1e06594a85f8b3eead513ceac83baab79fa695b
arafat_thabet <arafat.733011506@gmail.com>
2021-10-19 21:20:05 +0300

    update

--- application/config/config.php
+++ application/config/config.php
@@ -24,7 +24,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |
 */
 $config['base_url'] = 'https://stage.azooma.co/restaurant/';
-
+$config['sa_url']	= 'https://stage.azooma.co/restaurant/';
+$config['upload_url']	= '../uploads/';
 /*
 |--------------------------------------------------------------------------
 | Index File
