7c1c2bb05587361d1ab16e8b7d6374c4306d36a0
arafat_thabet <arafat.733011506@gmail.com>
2021-10-19 17:15:10 +0300

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Azooma/restaurant/';
+$config['base_url'] = 'https://stage.azooma.co/restaurant/';
 
 /*
 |--------------------------------------------------------------------------
